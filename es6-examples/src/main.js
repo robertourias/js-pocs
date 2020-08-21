324584834a9bf2a935075f9c3d7d0c93ef98b582
Roberto Nicoletti <roberto.urias@gmail.com>
2020-08-21 16:24:35 -0300

    criando o render da lista de repos

--- es6-examples/src/main.js
+++ es6-examples/src/main.js
@@ -3,6 +3,7 @@ class App {
         this.repositories = [];
 
         this.formElement = document.getElementById("repo-form");
+        this.listElement = document.getElementById("repo-list");
 
         this.registerHandlers();
     }
@@ -20,7 +21,35 @@ class App {
             html_url: 'http://github.com/rocketseat/rocketseat.com.br',
         });
 
-        console.log(this.repositories);
+        this.render();
+    }
+
+    render() {
+        this.listElement.innerHTML = '';
+
+        this.repositories.forEach(repo => {
+            let imgEl = document.createElement('img');
+            imgEl.setAttribute('src', repo.avatar_url);
+
+            let titleEl = document.createElement('strong');
+            titleEl.appendChild(document.createTextNode(repo.name));
+
+            let descriptionEl = document.createElement('p');
+            descriptionEl.appendChild(document.createTextNode(repo.description));
+
+            let linkEl = document.createElement('a');
+            linkEl.setAttribute('target', '_blank');
+            linkEl.setAttribute('href', repo.html_url);
+            linkEl.appendChild(document.createTextNode('Acessar'));
+
+            let listItemEl = document.createElement('li');
+            listItemEl.appendChild(imgEl);
+            listItemEl.appendChild(titleEl);
+            listItemEl.appendChild(descriptionEl);
+            listItemEl.appendChild(linkEl);
+
+            this.listElement.appendChild(listItemEl);
+        });
     }
 }
 
