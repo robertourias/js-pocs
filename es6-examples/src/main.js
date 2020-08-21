a27fdc89f533ec0af36486380a56a033c6e62534
Roberto Nicoletti <roberto.urias@gmail.com>
2020-08-21 16:13:45 -0300

    criando classe com registro de handler e addRepository

--- es6-examples/src/main.js
+++ es6-examples/src/main.js
@@ -1,1 +1,27 @@
-alert('ola');
+class App {
+    constructor() {
+        this.repositories = [];
+
+        this.formElement = document.getElementById("repo-form");
+
+        this.registerHandlers();
+    }
+
+    registerHandlers() {
+        this.formElement.onsubmit = event => this.addRepository(event);
+    }
+
+    addRepository(event) {
+        event.preventDefault();
+        this.repositories.push({
+            name: 'rocketseat',
+            description: 'Teste',
+            avatar_url: 'https://avatars0.githubusercontent.com/u/28929274?v=4',
+            html_url: 'http://github.com/rocketseat/rocketseat.com.br',
+        });
+
+        console.log(this.repositories);
+    }
+}
+
+new App();
